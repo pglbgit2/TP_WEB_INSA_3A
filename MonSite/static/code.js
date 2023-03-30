841c28ec00b1929d7b6fa9fd0cbbf9c9341f110e
pglbgit2 <117928188+pglbgit2@users.noreply.github.com>
2023-03-30 18:51:37 +0200

    Create code.js

---
+++ MonSite/static/code.js
@@ -0,0 +1,5 @@
+document.innerHTML += <link rel="stylesheet" href="APP_ROOT . inc/navbar.css">
+<div class="topnav">
+	<a href= "#">Explorer Articles</a> <a href= "#">Rechercher un article</a>
+	<a href= "#l">Mon Espace Blogueur</a> <a href= "#">Mentions légales</a>
+</div>
